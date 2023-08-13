f71a4233d9134236033d78b9a8db5c7473d775b4
JuanFelipe <maflay26@gmail.com>
2023-08-12 20:38:25 -0500

    pruebas

---
+++ folder/src/pages/Homepage.js
@@ -0,0 +1,32 @@
+import '../styles/Home-Style.css';
+
+function Homepage (){
+return(
+    <>
+
+<nav className='card'>
+    <ul >
+        <td >
+            asd
+        </td>
+        <td>
+            asd
+        </td>
+        <td >
+            asd
+        </td>
+        <td>
+            asd
+        </td>
+        <td >
+            asd
+        </td>
+        <td>
+            asd
+        </td>
+    </ul>
+</nav>
+    </>
+    )}
+
+export default Homepage;
